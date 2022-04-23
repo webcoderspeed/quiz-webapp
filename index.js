dba031f5d5cd670c45d9c170c500f2e4766b81e6
webcoderspeed <webcoderspeed@gmail.com>
2022-04-23 12:26:52 +0530

    remove server

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ const app = express();
 
 app.use(morgan('dev'));
 
-// socket connection
-setupSockets(app);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 1337;
+app.get('/', (req, res) => res.send('Server is running'));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// socket connection
+setupSockets(app);
