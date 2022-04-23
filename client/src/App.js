35489e3515d916c5bd8ff550b466a7d517d62960
webcoderspeed <webcoderspeed@gmail.com>
2022-04-23 12:39:07 +0530

    modified index

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080', {
+const socket = io('http://localhost:5000', {
   transport: ['websocket'],
 });
 
--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 import express from 'express';
 import morgan from 'morgan';
-import setupSockets from './socket.js';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { Aki } from 'aki-api';
 
 const app = express();
+const server = createServer(app);
 
 app.use(morgan('dev'));
 
-// socket connection
-setupSockets(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+io.on('connection', async (socket) => {
+  const aki = new Aki({
+    region: 'en',
+  });
+  await aki.start();
 
-const PORT = process.env.PORT || 1337;
+  socket.emit('question', {
+    question: aki.question,
+    answers: aki.answers,
+    progress: aki.progress,
+  });
 
-app.get('/', (req, res) => res.send('Server is running'));
+  socket.on('answer', async (data) => {
+    while (aki.progress <= 100) {
+      await aki.step(data.answer);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+      if (aki.progress >= 75 || aki.currentStep >= 79) {
+        await aki.win();
+        socket.emit('win', {
+          result: {
+            guessCount: aki.guessCount,
+            answers: aki.answers,
+          },
+        });
+        break;
+      }
+
+      console.log('progress:', aki.progress);
+      console.log('guessCount:', aki.guessCount);
+
+      const question = aki.question;
+      const answers = aki.answers;
+
+      socket.emit('question', {
+        question,
+        answers,
+        progress: aki.progress,
+      });
+      break;
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnect');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send(`Server is running `);
+});
+
+server.listen(5000, () => {
+  console.log(`Websocket server is listening on port: 5000`);
 });
