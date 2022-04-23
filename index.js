a118250866272729467551184b4e83cd2226cf30
webcoderspeed <webcoderspeed@gmail.com>
2022-04-23 12:28:50 +0530

    added server

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const app = express();
 
 app.use(morgan('dev'));
 
+// socket connection
+setupSockets(app);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const PORT = process.env.PORT || 1337;
+
 app.get('/', (req, res) => res.send('Server is running'));
 
-// socket connection
-setupSockets(app);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
